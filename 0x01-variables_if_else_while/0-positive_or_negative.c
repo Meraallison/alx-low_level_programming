558296c6f43a084032af8d8636be7eb33660d873
Okpadi-Allison Mera <meraallison5@gmail.com>
2022-09-09 18:51:10 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,7 @@
 
 /**
 * main - print if the number is positive, zero, or negative
+*
 * Description: using the main function
 * this program prints "Programming is positive, zero, or negative
 * Return: 0
@@ -15,6 +16,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	  /* your code goes there */
+
 	if (n > 0)
 	{
 		printf("%i is positive\n", n);
