ae224e43d4a92d58306f189d437dd42fbae1f9ef
Okpadi-Allison Mera <meraallison5@gmail.com>
2022-09-13 18:57:19 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
