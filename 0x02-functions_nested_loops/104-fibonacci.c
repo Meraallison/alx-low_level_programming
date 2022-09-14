a1e9a5218323d4c5ebfac4965de006eab5fe643b
Okpadi-Allison Mera <meraallison5@gmail.com>
2022-09-13 22:17:08 -0700

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,21 +12,20 @@ int main(void)
 	
 	j = 1;
 	k = 2;
-
+	
 	printf("%lu", j);
-
+	
 	for (i = 1; i < 91; i++)
 	{
 		printf(", %lu", k);
 		k = k + j;
 		j = k - j;
 	}
-
 	j1 = j / 1000000000;
 	j2 = j % 1000000000;
 	k1 = k / 1000000000;
 	k2 = k % 1000000000;
-
+	
 	for (i = 92; i < 99; ++i)
 	{
 		printf(", %lu", k1 + (k2 / 1000000000));
@@ -36,8 +35,7 @@ int main(void)
 		k2 = k2 + j2;
 		j2 = k2 - j2;
 	}
-
 	printf("\n");
-
+	
 	return (0);
 }
