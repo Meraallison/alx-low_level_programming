c03dc9696b6d76eba5cb05cd2070f242b581960b
Okpadi-Allison Mera <meraallison5@gmail.com>
2022-09-15 15:17:18 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 	int num = 1;
-
+	
 	while (num++ < 100)
 	{
 		if ((num % 3 == 0) && (num % 5 == 0))
@@ -39,6 +39,6 @@ int main(void)
 		}
 	}
 	printf("\n");
-
+	
 	return (0);
 }
