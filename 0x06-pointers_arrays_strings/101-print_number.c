2a8e7909cd48e6e6b622cc9060c8af3a274b4290
Okpadi-Allison Mera <meraallison5@gmail.com>
2022-09-22 19:29:53 -0700

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,8 +1,8 @@
 #include "main.h"
+
 /**
- * print_number - Prints an integer.
- * @n: integer to be printed.
- *
+ * print_number - Prints an integer;
+ * @n: integer to be printed;
  */
 void print_number(int n)
 {
