de9f99f5994bc06b1b10d945c0fd10e93e459ccb
Okpadi-Allison Mera <meraallison5@gmail.com>
2022-09-26 19:05:07 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef FILE_MAIN
+#define File_MAIN
+
+/**
+ * void prototypes()
+ * int prototypes()
+ */
+
+int _putchar(char);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
+#endif /*MAIN_H*/
